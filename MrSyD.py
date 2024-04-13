ca56bf6cc0e4d570e555c113003f0a5c942cf508
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-04-13 09:04:47 +0530

    Create MrSyD.py

---
+++ MrSyD.py
@@ -0,0 +1,20 @@
+import logging
+from pyrogram.errors import UserNotParticipant
+from database import db
+from info import AUTH_CHANNEL
+
+
+async def is_req_subscribed(bot, query):
+    if await db.find_join_req(query.from_user.id):
+        return True
+    try:
+        user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
+    except UserNotParticipant:
+        pass
+    except Exception as e:
+        logger.exception(e)
+    else:
+        if user.status != enums.ChatMemberStatus.BANNED:
+            return True
+
+    return False
