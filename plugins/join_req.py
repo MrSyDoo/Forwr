dc914b09507e675454ff767e20d170665c99768d
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-04-13 09:56:43 +0530

    Update join_req.py

--- plugins/join_req.py
+++ plugins/join_req.py
@@ -1,15 +1,14 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import ChatJoinRequest
 from database import db
-from config import Config
-from info import AUTH_CHANNEL
+from info import AUTH_CHANNEL, ADMINS
 
 @Client.on_chat_join_request(filters.chat(AUTH_CHANNEL))
 async def join_reqs(client, message: ChatJoinRequest):
   if not await db.find_join_req(message.from_user.id):
     await db.add_join_req(message.from_user.id)
 
-@Client.on_message(filters.command("delreq") & filters.private & filters.user(BOT_OWNER_ID))
+@Client.on_message(filters.command("delreq") & filters.private & filters.user(ADMINS))
 async def del_requests(client, message):
     await db.del_join_req()    
     await message.reply("<b>⚙ ꜱᴜᴄᴄᴇꜱꜱғᴜʟʟʏ ᴄʜᴀɴɴᴇʟ ʟᴇғᴛ ᴜꜱᴇʀꜱ ᴅᴇʟᴇᴛᴇᴅ</b>")
