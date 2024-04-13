287280d41c2266f685b9604436971e8d91a959d3
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-04-13 18:16:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ if __name__ == "__main__" :
         await AceBot.start()
         bot_info  = await AceBot.get_me()
         LOGGER.info(f"<--- @{bot_info.username} Started (c) ACE --->")
-        await AceBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+        await AceBot.send_message(chat_id=LOG_CHANNEL, text=Script.RESTART_TXT.format(today, time))
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
