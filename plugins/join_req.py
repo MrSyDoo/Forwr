fe863af85e115e45214a9e4a5e98293ac5fb9130
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-04-15 16:50:06 +0530

    Update join_req.py

--- plugins/join_req.py
+++ plugins/join_req.py
@@ -13,7 +13,12 @@ async def del_requests(client, message):
     await db.del_join_req()    
     await message.reply("<b>⚙ ꜱᴜᴄᴄᴇꜱꜱғᴜʟʟʏ ᴄʜᴀɴɴᴇʟ ʟᴇғᴛ ᴜꜱᴇʀꜱ ᴅᴇʟᴇᴛᴇᴅ</b>")
   
-@Client.on_chat_join_request(filters.chat(AUTH_CHANNEL))
+@Client.on_chat_join_request(filters.chat(SYD_CHANNEL))
 async def join_reqsa(client, message: ChatJoinRequest):
   if not await db.find_join_req(message.from_user.id):
     await db.add_join_req(message.from_user.id)
+
+@Client.on_message(filters.command("delreqa") & filters.private & filters.user(ADMINS))
+async def del_requests(client, message):
+    await db.del_join_req()    
+    await message.reply("<b>⚙ ꜱᴜᴄᴄᴇꜱꜱғᴜʟʟʏ ᴄʜᴀɴɴᴇʟ ʟᴇғᴛ ᴜꜱᴇʀꜱ ᴅᴇʟᴇᴛᴇᴅ</b>")
