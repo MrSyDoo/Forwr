b440b854e5b247cbc77c1967630562c954ba97d2
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-04-15 15:32:56 +0530

    Update MrSyD.py

--- MrSyD.py
+++ MrSyD.py
@@ -2,7 +2,7 @@ import logging
 from pyrogram import enums
 from pyrogram.errors import UserNotParticipant
 from database import db
-from info import AUTH_CHANNEL
+from info import AUTH_CHANNEL, BOTCRACKER_CHNL, SYD_CHANNEL
 
 
 async def is_req_subscribed(bot, query):
@@ -26,6 +26,21 @@ async def is_reqa_subscribed(bot, query):
         return True
     try:
         user = await bot.get_chat_member(SYD_CHANNEL, query.from_user.id)
+    except UserNotParticipant:
+        pass
+    except Exception as e:
+        logger.exception(e)
+    else:
+        if user.status != enums.ChatMemberStatus.BANNED:
+            return True
+
+    return False
+
+async def is_reqb_subscribed(bot, query):
+    if await db.find_join_req(query.from_user.id):
+        return True
+    try:
+        user = await bot.get_chat_member(BOTCRACKER_CHNL, query.from_user.id)
     except UserNotParticipant:
         pass
     except Exception as e:
