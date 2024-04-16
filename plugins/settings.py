5535b7abfe468b1a0df06b72e6a85da8f79cb2c7
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-04-16 07:33:20 +0530

    Update settings.py

--- plugins/settings.py
+++ plugins/settings.py
@@ -180,7 +180,6 @@ async def settings_query(bot, query):
               ]
         ]
      await query.message.edit_text(
-         user_id=query.from_user.id,
          text="ᴊᴏɪɴ ᴏᴜʀ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ᴛʀʏ ᴀɢᴀɪɴ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ʀᴇǫᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇ.",
          reply_markup=InlineKeyboardMarkup(syd),
          parse_mode=enums.ParseMode.MARKDOWN
