8025166b205eb6e374b39e58fed2f926855aae86
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-05-09 18:26:46 +0530

    Update settings.py

--- plugins/settings.py
+++ plugins/settings.py
@@ -4,7 +4,7 @@ from script import Script
 from info import SYD_CHANNEL
 from MrSyD import is_reqa_subscribed
 from pyrogram import Client, filters, enums
-from .test import get_configs, update_configs, CLIENT, parse_buttons
+from .test import get_configs, update_configs, CLIENT, parse_buttons, start_clone_bot
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import ChatAdminRequired
 
